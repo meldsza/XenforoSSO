60650741bbb2c5bfac16f69b7e9b4c7bd630bf70
Melroy Neil Dsouza <meldsza@gmail.com>
2017-09-01 20:58:19 +0530

    Update Setup.php

--- Setup.php
+++ Setup.php
@@ -9,10 +9,6 @@ class Setup extends AbstractSetup
 {
 	public function install(array $stepParams = [])
 	{
-		$db = \XF::db();
-		$db->query("INSERT IGNORE INTO xf_connected_account_provider (provider_id, provider_class, display_order, options)
-		VALUES ('Meldsza', 'Meldsza\\\\XenforoSSO\\\\ConnectedAccount\\\\Provider\\\\Meldsza', 5, '')");
-
 	}
 
 	public function upgrade(array $stepParams = [])
@@ -22,6 +18,5 @@ class Setup extends AbstractSetup
 
 	public function uninstall(array $stepParams = [])
 	{
-		$db->query("DELETE FROM xf_connected_account_provider where provider_id = 'Meldsza'");
 	}
 }
