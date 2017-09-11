c7d18e141cb54d77438f7d903d54c39c513e92e6
Melroy Neil Dsouza <meldsza@gmail.com>
2017-09-11 07:39:42 +0530

    Update case 2

--- LoginSSO.php
+++ LoginSSO.php
@@ -77,8 +77,8 @@ class LoginSSO extends Login
                     );
                 } else {
                     \XF::db()->insert('xf_user_field_value',
-                    ['field_id' => $this->options()->sso_external_id,'user_id' =>$payload["external_id"],
-                    'field_value = ?', $user->user_id
+                    ['field_id' => $this->options()->sso_external_id,'field_value' =>$payload["external_id"],
+                    'user_id', $user->user_id
                     );
                 }
             }
